edd09809e34c991648bdc43c0631f452392c6752
hsjun <hippo8976@gmail.com>
2025-09-16 16:58:38 +0900

    에러 유틸 타입 변경

--- src/utils/error-util.ts
+++ src/utils/error-util.ts
@@ -1,4 +1,5 @@
 import { HttpError } from '../common/errors/error-type';
 
-export const isErrorInstanceOfHttp = (error: any): error is HttpError => error instanceof HttpError;
-export const isErrorInstanceOfNode = (error: any): error is Error => error instanceof Error;
+export const isErrorInstanceOfHttp = (error: unknown): error is HttpError =>
+  error instanceof HttpError;
+export const isErrorInstanceOfNode = (error: unknown): error is Error => error instanceof Error;
