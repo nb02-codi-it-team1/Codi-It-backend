58a81fcb3542402cc692ee2667a8292054e4aeb4
yun-heewon <union00916@gmail.com>
2025-10-23 14:40:07 +0900

    refactor: 상점 정보를 조회하도록 인가 로직 강화

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { ForbiddenError } from '../common/errors/error-type';
 import { UserType } from '@prisma/client';
+import prisma from 'src/common/prisma/client';
 
 interface AuthUser {
   id: string;
@@ -8,9 +9,18 @@ interface AuthUser {
   type: UserType;
 }
 
+interface AuthorizedRequest extends Request {
+  user?: AuthUser;
+  storeId?: string;
+}
+
 // 판매자만 허용
-export const authorizeSeller = (req: Request, res: Response, next: NextFunction) => {
-  const user = req.user as AuthUser | undefined;
+export const authorizeSeller = async (
+  req: AuthorizedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = req.user;
 
   if (!user) {
     return next(new ForbiddenError('로그인이 필요합니다.'));
@@ -20,7 +30,20 @@ export const authorizeSeller = (req: Request, res: Response, next: NextFunction)
     return next(new ForbiddenError('판매자 권한이 필요합니다.'));
   }
 
-  return next();
+  try {
+    const sellerStore = await prisma.store.findUnique({
+      where: { userId: user.id },
+      select: { id: true },
+    });
+
+    if (!sellerStore) {
+      return next(new ForbiddenError('연결된 상점 정보가 없습니다.'));
+    }
+    req.storeId = sellerStore.id;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 };
 
 // 구매자만 허용
